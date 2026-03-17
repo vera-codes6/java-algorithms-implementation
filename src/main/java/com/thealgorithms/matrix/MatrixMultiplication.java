5cfd0d2f27fdef70dea386171a9bb5ecc5a22f18
vera-codes6 <vera.codes6@gmail.com>
2026-03-17 04:41:53 -0400

    update matrix multi logic

--- src/main/java/com/thealgorithms/matrix/MatrixMultiplication.java
+++ src/main/java/com/thealgorithms/matrix/MatrixMultiplication.java
@@ -44,12 +44,15 @@ public final class MatrixMultiplication {
             throw new IllegalArgumentException("Input matrices must not be empty");
         }
 
+
         // Validate the matrix dimensions
         if (matrixA[0].length != matrixB.length) {
             throw new IllegalArgumentException("Matrices cannot be multiplied: incompatible dimensions.");
         }
 
+
         int rowsA = matrixA.length;
+
         int colsA = matrixA[0].length;
         int colsB = matrixB[0].length;
 
@@ -60,7 +63,9 @@ public final class MatrixMultiplication {
         for (int i = 0; i < rowsA; i++) {
             for (int j = 0; j < colsB; j++) {
                 for (int k = 0; k < colsA; k++) {
+
                     result[i][j] += matrixA[i][k] * matrixB[k][j];
+
                 }
             }
         }
